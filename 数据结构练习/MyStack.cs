d187a7f81dfd88af7e9964592fd3836fec5e4897
DESKTOP-82AQ7T2\Yon <1161814672@qq.com>
2018-09-18 23:58:52 +0800

    new Code File

---
+++ 数据结构练习/MyStack.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp5
+{
+
+    public interface IStack<T>
+    {
+        int Count { get; }
+        int GetLength();//求栈的长度
+        bool IsEmpty();
+        void Clear();//清空操作
+
+        void Push(T item);
+
+        T Pop();//出栈操作
+        T Peek();//取栈顶元素
+    }
+    //顺序栈
+    class MyStack<T> : IStack<T>
+    {
+        T[] a;
+        int top = -1;
+        int size = 10;
+        
+        public MyStack()
+        {
+            a = new T[size];
+        }
+        public int Count => top+1;
+       
+        public void Clear()
+        {
+            top = -1;
+        }
+
+        public int GetLength()
+        {
+            return top + 1;
+        }
+
+        public bool IsEmpty()
+        {
+            if (top==-1)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public T Peek()
+        {
+            if (top >= 0)
+            {
+                T temp = a[top];
+                return temp;
+            }
+            else
+            {
+                Console.WriteLine("栈为空无法删除");
+                return default(T);
+            }
+        }
+
+        public T Pop()
+        {
+            if (top>=0)
+            {
+                T temp = a[top];
+                //a[top] = default(T);
+                top--;
+                return temp;
+            }
+            else
+            {
+                Console.WriteLine("栈为空无法删除");
+                return default(T);
+            }
+        }
+        /// <summary>
+        /// 入栈
+        /// </summary>
+        /// <param name="item"></param>
+        public void Push(T item)
+        {
+            //自动扩容
+            if (top + 1>this.size-1)
+            {
+                this.size = this.size * 2;//容量扩大一倍
+                T[] temp = new T[size];
+
+                for (int i = 0; i < a.Length; i++)
+                {
+                    temp[i] = a[i]; //将数组a 的值都赋值过去
+                }
+                a = new T[size];//重新构建一个数组a
+                for (int i = 0; i < temp.Length; i++)
+                {
+                    a[i] = temp[i]; //扩容后将数据取回来
+                }
+                a[top + 1] = item;
+                top++;
+            }
+            else
+            {
+                a[top + 1] = item;
+                top++;
+            }
+          
+        }
+    }
+}
---
+++ 数据结构练习/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 15;
+            Console.WriteLine("15");
+            MyStack<int> myStack = new MyStack<int>();
+            myStack.Push(1);
+            myStack.Push(2);
+            myStack.Push(3);
+            myStack.Push(4);
+            myStack.Push(5);
+            myStack.Push(6);
+            myStack.Push(7);
+            myStack.Push(8);
+            myStack.Push(9);
+            myStack.Push(10);
+            myStack.Push(11);
+            myStack.Push(12);
+            myStack.Push(10);
+            myStack.Push(10);
+            myStack.Push(10);
+            myStack.Push(10);
+            myStack.Push(10);
+            myStack.Push(10);
+            myStack.Push(10);
+            myStack.Push(10);
+            myStack.Push(10);
+            myStack.Push(10);
+            myStack.Push(10);
+            myStack.Push(10);
+            myStack.Push(10);
+            myStack.Push(10);
+            myStack.Push(10);
+            myStack.Push(10);
+            myStack.Push(10);
+            myStack.Push(10);
+            myStack.Push(10);
+            myStack.Push(10);
+            myStack.Push(10);
+            myStack.Push(10);
+            myStack.Push(10);
+            myStack.Push(10);
+            myStack.Push(10);
+            myStack.Push(10);
+            myStack.Push(10);
+            myStack.Push(10);
+            myStack.Push(10);
+            myStack.Push(10);
+            myStack.Push(10);
+            myStack.Push(10);
+            myStack.Push(10);
+            myStack.Push(10);
+            myStack.Push(10);
+            myStack.Push(10);
+            myStack.Push(10);
+            myStack.Push(10);
+            myStack.Push(10);
+            myStack.Push(999);
+            while (myStack.IsEmpty()!=true)
+            {
+                Console.WriteLine(myStack.Pop()+" Count: "+ myStack.Count);
+            }
+            Console.ReadKey();
+        }
+    }
+}
